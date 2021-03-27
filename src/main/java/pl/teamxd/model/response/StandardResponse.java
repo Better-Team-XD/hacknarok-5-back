15a649b99518a6582f02d7d57993fe60c1413470
Jakub Solecki <jakub.solecki@onet.com.pl>
2021-03-27 19:04:01 +0100

    responses

---
+++ src/main/java/pl/teamxd/model/response/StandardResponse.java
@@ -0,0 +1,12 @@
+package pl.teamxd.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class StandardResponse {
+    private final Type type;
+    private final String message;
+    private final Object data;
+}
